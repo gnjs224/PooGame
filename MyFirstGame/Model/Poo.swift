92eef1c08d038f2b714fa4d1a4dc69ddc0813904
김지훈 <ben224@naver.com>
2022-08-30 02:27:06 +0900

    [수정] Poo.swift 위치 수정

--- MyFirstGame/Model/Poo.swift
+++ MyFirstGame/Model/Poo.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-
 struct Poo {
     let pooImageView: UIImageView
     var x: CGFloat
@@ -14,15 +13,13 @@ struct Poo {
     var width: CGFloat
     var height: CGFloat
     var speed: CGFloat
-    init(x: CGFloat, y: CGFloat, spped: CGFloat) {
+    init(x: CGFloat, y: CGFloat, speed: CGFloat) {
         self.x = x
         self.y = y
         self.width = 30
         self.height = 30
-        self.speed = spped
+        self.speed = speed
         pooImageView = UIImageView(frame: CGRect(x: x, y: y, width: self.width, height: self.height))
-        pooImageView.image = UIImage(named: "poo")
-
+        pooImageView.image = UIImage(named: Asset.ETC.poo)
     }
-
 }
